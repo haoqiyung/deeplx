c0934912ac24e50cd00e21ccd54b4be856bb0420
haoqiyung <101010688+haoqiyung@users.noreply.github.com>
2023-03-05 13:05:03 +0800

    Create .cross_compile.sh

---
+++ .cross_compile.sh
@@ -0,0 +1,29 @@
+
+
+set -e
+
+DIST_PREFIX="deeplx"
+DEBUG_MODE=${2}
+TARGET_DIR="dist"
+PLATFORMS="darwin/amd64 darwin/arm64 linux/386 linux/amd64 linux/arm64 linux/mips openbsd/amd64 openbsd/arm64 freebsd/amd64 freebsd/arm64 windows/386 windows/amd64"
+
+rm -rf ${TARGET_DIR}
+mkdir ${TARGET_DIR}
+
+for pl in ${PLATFORMS}; do
+    export GOOS=$(echo ${pl} | cut -d'/' -f1)
+    export GOARCH=$(echo ${pl} | cut -d'/' -f2)
+    export TARGET=${TARGET_DIR}/${DIST_PREFIX}_${GOOS}_${GOARCH}
+    if [ "${GOOS}" == "windows" ]; then
+        export TARGET=${TARGET_DIR}/${DIST_PREFIX}_${GOOS}_${GOARCH}.exe
+    fi
+
+    echo "build => ${TARGET}"
+    if [ "${DEBUG_MODE}" == "debug" ]; then
+        CGO_ENABLED=0 go build -trimpath -gcflags "all=-N -l" -o ${TARGET} \
+            -ldflags "-w -s" main.go
+    else
+        CGO_ENABLED=0 go build -trimpath -o ${TARGET} \
+            -ldflags "-w -s" main.go
+    fi
+done
